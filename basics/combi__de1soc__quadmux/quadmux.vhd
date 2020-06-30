28ac72c2910eda78f9cf68fafd39b519b19f8e49
Lothar Rubusch <l.rubusch@gmx.ch>
2020-06-30 22:00:54 +0200

    basics: another basics project, select approach

--- basics/combi__de1soc__quadmux/quadmux.vhd
+++ basics/combi__de1soc__quadmux/quadmux.vhd
@@ -1,4 +1,4 @@
---                                                                                                                      
+--
 --  A[3..0] +----------+
 --  --------| QUAD     | C[3..0]
 --  B[3..0] | 2-INPUT  |-------->
@@ -14,19 +14,20 @@ USE IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.NUMERIC_STD.ALL;
 
 ENTITY QUADMUX_ENT IS
-PORT( A   IN : STD_LOGIC_VECTOR(3 DOWNTO 0)
-	; B   IN : STD_LOGIC_VECTOR(3 DOWNTO 0)
-	; SEL IN : STD_LOGIC
-	; C  OUT : STD_LOGIC_VECTOR(3 DOWNTO 0)
-);
+PORT( A   : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+      B   : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+      SEL : IN STD_LOGIC;
+      C   : OUT STD_LOGIC_VECTOR(3 DOWNTO 0));
 END ENTITY QUADMUX_ENT;
 
+
 ARCHITECTURE QUADMUX_ARCH OF QUADMUX_ENT IS
-
 BEGIN
-	C <= A WHEN SEL = '0' ELSE B;
+-- A FIRST APPROACH (WORKING)
+--	C <= A WHEN SEL = '0' ELSE B;
 
---	WITH SEL SELECT
---	C <= A WHEN '0',
---		 B WHEN OTHERS;
+-- SYNTACTICALLY MORE ELEGANT TO USE SELECT IN CONTEXT OF A MULTIPLEXER
+  WITH SEL SELECT
+    C <= A WHEN '0',
+         B WHEN OTHERS;
 END ARCHITECTURE QUADMUX_ARCH;
