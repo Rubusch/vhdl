2a20942a0f1b6e8d040a3836770d6aa8f3e0d257
Lothar Rubusch <l.rubusch@gmx.ch>
2020-07-01 13:11:37 +0200

    basics: make sevenseg hex and drive one digit

--- basics/combi__de1soc__sevenseg/sevenseg.vhd
+++ basics/combi__de1soc__sevenseg/sevenseg.vhd
@@ -1,30 +1,38 @@
+--
+--             +-------------+
+-- data[3..0]  | seven       | seg[6..0]
+-- ------/---->| seg display |---/--->
+--             | controller  |
+--             +-------------+
+--
+
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
-USE IEEE.STD_NUMERIC.ALL;
+USE IEEE.NUMERIC_STD.ALL;
 
 ENTITY SEVENSEG_ENT IS
 PORT( DATA : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
-	SEG : OUT STD_LOGIC_VECTOR(13 DOWNTO 0));
+	SEG : OUT STD_LOGIC_VECTOR(6 DOWNTO 0));
 END ENTITY SEVENSEG_ENT;
 
 ARCHITECTURE SEVENSEG_ARCH OF SEVENSEG_ENT IS
 BEGIN
 	WITH DATA SELECT
-		SEG <= "00000001111110" WHEN "0000",  -- 00
-			   "00000000110000" WHEN "0001",  -- 01
-			   "00000001101101" WHEN "0001",  -- 02
-			   "00000001111001" WHEN "0001",  -- 03
-			   "00000000110011" WHEN "0001",  -- 04
-			   "00000001011011" WHEN "0001",  -- 05
---			   "0000000" WHEN "0001",  -- 06
---			   "0000000" WHEN "0001",  -- 07
---			   "0000000" WHEN "0001",  -- 08
---			   "0000000" WHEN "0001",  -- 09
---			   "01100001111110" WHEN "0001",  -- 10
---			   "" WHEN "0001",  -- 11
---			   "" WHEN "0001",  -- 12
---			   "" WHEN "0001",  -- 13
---			   "" WHEN "0001",  -- 14
---			   "" WHEN "0001",  -- 15
-			   "11111101111110" WHEN OTHERS;
+		SEG <= "1000000" WHEN "0000",  -- 00
+			   "1111001" WHEN "0001",  -- 01
+			   "0100100" WHEN "0010",  -- 02
+			   "0110000" WHEN "0011",  -- 03
+			   "0011001" WHEN "0100",  -- 04
+			   "0010010" WHEN "0101",  -- 05
+			   "0000010" WHEN "0110",  -- 06
+			   "1111000" WHEN "0111",  -- 07
+			   "0000000" WHEN "1000",  -- 08
+			   "0010000" WHEN "1001",  -- 09
+			   "0001000" WHEN "1010",  -- A (10)
+			   "0000011" WHEN "1011",  -- B (11)
+			   "1000110" WHEN "1100",  -- C (12)
+			   "0100001" WHEN "1101",  -- D (13)
+			   "0000110" WHEN "1110",  -- E (14)
+			   "0001110" WHEN "1111",  -- F (15)
+			   "0000110" WHEN OTHERS;  -- 0
 END ARCHITECTURE SEVENSEG_ARCH;
