af72ace409874c94797551cf55cd880c542e4428
Lothar Rubusch <l.rubusch@gmx.ch>
2020-07-03 01:29:25 +0200

    basics: shifter - implemented

--- basics/combi__de1soc__shifter/shifter.vhd
+++ basics/combi__de1soc__shifter/shifter.vhd
@@ -1,6 +1,12 @@
 --
---
---
+--          +---------+
+--  D(7..0) |         | S(7..0)
+-- ---------| 1-BIT   |---------
+--          | SHIFTER |
+--          |         |
+--          +---------+
+--  C            |
+-- --------------+
 
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
@@ -15,5 +21,12 @@ END ENTITY SHIFTER_ENT;
 
 ARCHITECTURE SHIFTER_ARCH OF SHIFTER_ENT IS
 BEGIN
-
+    S(0) <= NOT C AND D(1);
+    S(1) <= (D(0) AND C) OR (NOT C AND D(2));
+    S(2) <= (D(1) AND C) OR (NOT C AND D(3));
+    S(3) <= (D(2) AND C) OR (NOT C AND D(4));
+    S(4) <= (D(3) AND C) OR (NOT C AND D(5));
+    S(5) <= (D(4) AND C) OR (NOT C AND D(6));
+    S(6) <= (D(5) AND C) OR (NOT C AND D(7));
+    S(7) <= D(6) AND C;
 END ARCHITECTURE SHIFTER_ARCH;
