dd86ea2e1982ce68830e2d5a80bf7509b7e9ddcb
Lothar Rubusch <l.rubusch@gmx.ch>
2020-07-11 20:41:58 +0200

    basics: testbench05 - update

--- basics/testbench__combinational__05__write-data-file/halfadder_tb.vhd
+++ basics/testbench__combinational__05__write-data-file/halfadder_tb.vhd
@@ -2,22 +2,46 @@
 
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
+USE STD.TEXTIO.ALL;
+USE IEEE.STD_LOGIC_TEXTIO.ALL; -- writing STD_LOGIC etc.
 
 ENTITY HALFADDER_ENT_TB IS
 END HALFADDER_ENT_TB;
 
 ARCHITECTURE TB OF HALFADDER_ENT_TB IS
-    SIGNAL A, B : STD_LOGIC;       -- inputs
-    SIGNAL SUM, CARRY : STD_LOGIC; -- outputs
-BEGIN
-    -- connecting testbench signals with half_adder.vhd
-    UUT : ENTITY WORK.HALFADDER_ENT PORT MAP (A => A, B=> B, SUM => SUM, CARRY => CARRY);
+    SIGNAL A : STD_LOGIC;
 
-    -- inputs
-    -- 00 at 0 ns
-    -- 01 at 20 ns, as b is 0 at 20 ns and a is changed to 1 at 20 ns
-    -- 10 at 40 ns
-    -- 11 at 60 ns
-    A <= '0', '1' AFTER 20 NS, '0' AFTER 40 NS, '1' AFTER 60 NS;
-    B <= '0', '1' AFTER 40 NS;
+    FILE OUTPUT_BUF : TEXT; -- TEXT is a reserved word
+
+BEGIN
+
+    TB1 : PROCESS
+    VARIABLE WRITE_COL_TO_OUTPUT_BUF : LINE; -- LINE is a reserved word
+    VARIABLE B : INTEGER := 40;
+
+    BEGIN
+
+        A <= '1'; -- assign value to A
+        WAIT FOR 20 NS;
+
+        -- if ModelSim project is created, provide the relative path
+        FILE_OPEN(OUTPUT_BUF, "../../data.out", WRITE_MODE);
+        -- else provide the absolute path
+--        FILE_OPEN(OUTPUT_BUF, "/media/user/develop/github__vhdl/basics/testbench__combinational__05__write-data-file/data.out", WRITE_MODE);
+
+        WRITE(WRITE_COL_TO_OUTPUT_BUF, STRING'("printing values"));
+        WRITELINE(OUTPUT_BUF, WRITE_COL_TO_OUTPUT_BUF); -- write in line 1
+
+        WRITE(WRITE_COL_TO_OUTPUT_BUF, STRING'("A = "));
+        WRITE(WRITE_COL_TO_OUTPUT_BUF, A);
+        WRITE(WRITE_COL_TO_OUTPUT_BUF, STRING'("B = "));
+        WRITE(WRITE_COL_TO_OUTPUT_BUF, B);
+        WRITELINE(OUTPUT_BUF, WRITE_COL_TO_OUTPUT_BUF); -- write in line 2
+
+        WRITE(WRITE_COL_TO_OUTPUT_BUF, STRING'("THANX"));
+        WRITELINE(OUTPUT_BUF, WRITE_COL_TO_OUTPUT_BUF); -- write in line 3
+
+        FILE_CLOSE(OUTPUT_BUF);
+        WAIT; -- indefinitely suspend process
+    END PROCESS;
 END TB;
