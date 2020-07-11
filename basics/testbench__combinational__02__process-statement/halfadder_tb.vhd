8b907517d31e863739189f69f83d7b1a63189c7c
Lothar Rubusch <l.rubusch@gmx.ch>
2020-07-11 11:26:39 +0200

    basics: testbench02 - setup testbench

--- basics/testbench__combinational__02__process-statement/halfadder_tb.vhd
+++ basics/testbench__combinational__02__process-statement/halfadder_tb.vhd
@@ -21,20 +21,38 @@ BEGIN
         WAIT FOR PERIOD;
         ASSERT ((SUM = '0') AND (CARRY = '0')) --EXPECTED OUTPUT
         -- ERROR WILL BE REPORTED IF SUM OR CARRY IS NOT '0'
-        REPORT "TEST FAILED FOR INPUT COMBINATION 01" SEVERITY ERROR;
-        
+            REPORT "TEST FAILED FOR INPUT COMBINATION 00"
+            SEVERITY ERROR;
+
         A <= '0';
         B <= '1';
         WAIT FOR PERIOD;
-        ASSERT ((SUM = '1') AND (CARRY = '0'));
-        REPORT "TEST FAILED FOR INPUT COMBINATION 01" SEVERITY ERROR;
-        
-        
-    -- inputs
-    -- 00 at 0 ns
-    -- 01 at 20 ns, as b is 0 at 20 ns and a is changed to 1 at 20 ns
-    -- 10 at 40 ns
-    -- 11 at 60 ns
-    A <= '0', '1' AFTER 20 NS, '0' AFTER 40 NS, '1' AFTER 60 NS;
-    B <= '0', '1' AFTER 40 NS;
+        ASSERT ((SUM = '1') AND (CARRY = '0'))
+            REPORT "TEST FAILED FOR INPUT COMBINATION 01"
+            SEVERITY ERROR;
+
+        A <= '1';
+        B <= '0';
+        WAIT FOR PERIOD;
+        ASSERT ((SUM = '1') AND (CARRY = '0'))
+            REPORT "TEST FAILED FOR INPUT COMBINATION 10"
+            SEVERITY ERROR;
+
+        A <= '1';
+        B <= '1';
+        WAIT FOR PERIOD;
+        ASSERT ((SUM = '0') AND (CARRY = '1'))
+            REPORT "TEST FAILED FOR INPUT COMBINATION 11"
+            SEVERITY ERROR;
+
+        -- FAIL TEST
+        A <= '0';
+        B <= '1';
+        WAIT FOR PERIOD;
+        ASSERT ((SUM = '0') AND (CARRY = '0'))
+            REPORT "FAIL TEST (FAIL EXPECTED)"
+            SEVERITY ERROR;
+
+        WAIT; -- INDEFINITELY SUSPEND PROCESS
+    END PROCESS;
 END TB;
