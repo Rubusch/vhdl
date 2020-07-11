d49a34a062a9b0899f51b39730d0f434bb5480c7
Lothar Rubusch <l.rubusch@gmx.ch>
2020-07-11 16:02:01 +0200

    README: update

--- basics/testbench__combinational__04__read-file-data/halfadder_tb.vhd
+++ basics/testbench__combinational__04__read-file-data/halfadder_tb.vhd
@@ -1,23 +1,53 @@
 -- HALFADDER_TB.VHD
+--
+-- IMPORTANT: DON'T FORGET TO ADD data.txt TO THE PROJECT!!!
 
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
+USE STD.TEXTIO.ALL;
+USE IEEE.STD_LOGIC_TEXTIO.ALL; -- require for writing/reading std_logic etc.
 
 ENTITY HALFADDER_ENT_TB IS
 END HALFADDER_ENT_TB;
 
 ARCHITECTURE TB OF HALFADDER_ENT_TB IS
     SIGNAL A, B : STD_LOGIC;       -- inputs
-    SIGNAL SUM, CARRY : STD_LOGIC; -- outputs
-BEGIN
-    -- connecting testbench signals with half_adder.vhd
-    UUT : ENTITY WORK.HALFADDER_ENT PORT MAP (A => A, B=> B, SUM => SUM, CARRY => CARRY);
+    SIGNAL C : STD_LOGIC_VECTOR(1 DOWNTO 0); -- NEW: output here as STD_LOGIC_VECTOR
 
-    -- inputs
-    -- 00 at 0 ns
-    -- 01 at 20 ns, as b is 0 at 20 ns and a is changed to 1 at 20 ns
-    -- 10 at 40 ns
-    -- 11 at 60 ns
-    A <= '0', '1' AFTER 20 NS, '0' AFTER 40 NS, '1' AFTER 60 NS;
-    B <= '0', '1' AFTER 40 NS;
+    -- buffer for storing the text from input read-file
+    FILE INPUT_BUF : TEXT; -- TEXT is a reserved word!!!
+
+BEGIN
+
+    TB1 : PROCESS
+        VARIABLE READ_COL_FROM_INPUT_BUF : LINE;           -- reads lines one by one from INPUT_BUF
+        VARIABLE VAL_COL1, VAL_COL2 : STD_LOGIC;           -- to save COL1 and COL2 values of 1 bit
+        VARIABLE VAL_COL3 : STD_LOGIC_VECTOR(1 DOWNTO 0);  -- to save COL3 value of 2 bit
+        VARIABLE VAL_SPACE : CHARACTER;                    -- for spaces between data in file
+
+    BEGIN
+
+        -- if ModelSim project is created, then provide the relative path of the input file
+        -- (i.e. data.txt) with respect to main project folder
+        FILE_OPEN(INPUT_BUF, "../../data.txt", READ_MODE);
+        -- else provide the complete path for the input file as shown below
+--        FILE_OPEN(INPUT_BUF, "/media/user/develop/github__vhdl/basics/testbench__combinational__04__read-file-data/data.txt", READ_MODE);
+
+        WHILE NOT ENDFILE(INPUT_BUF) LOOP
+            READLINE(INPUT_BUF, READ_COL_FROM_INPUT_BUF);
+            READ(READ_COL_FROM_INPUT_BUF, VAL_COL1);
+            READ(READ_COL_FROM_INPUT_BUF, VAL_SPACE);      -- read in space character
+            READ(READ_COL_FROM_INPUT_BUF, VAL_COL2);
+            READ(READ_COL_FROM_INPUT_BUF, VAL_SPACE);      -- read in space character
+            READ(READ_COL_FROM_INPUT_BUF, VAL_COL3);
+
+            -- pass the read values to signals
+            A <= VAL_COL1;
+            B <= VAL_COL2;
+            C <= VAL_COL3;
+            WAIT FOR 20 NS; -- display results every 20ns
+        END LOOP;
+        FILE_CLOSE(INPUT_BUF);
+        WAIT;
+    END PROCESS;
 END TB;
