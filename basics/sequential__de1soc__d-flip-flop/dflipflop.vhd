d4a12fe987e28e53587cfdce81a1e9e20372a853
Lothar Rubusch <l.rubusch@gmx.ch>
2020-07-14 12:25:48 +0200

    basics: d-flipflop - add ENA input

--- basics/sequential__de1soc__d-flip-flop/dflipflop.vhd
+++ basics/sequential__de1soc__d-flip-flop/dflipflop.vhd
@@ -9,6 +9,11 @@ USE IEEE.NUMERIC_STD.ALL;
 ENTITY DFLIPFLOP_ENT IS
 PORT( CLK : IN STD_LOGIC
     ; RST : IN STD_LOGIC
+    ; ENA : IN STD_LOGIC
+    -- ENA: enable e.g. if we want to change the output of the 
+    -- D-flip-flop on every 10th-clock, then we can set the
+    -- enable to '1' for every 10thclock and '0' for rest of
+    -- the clocks
 	; D : IN STD_LOGIC
 	; Q : OUT STD_LOGIC := '0'
 	; QBAR : OUT STD_LOGIC := '1'
@@ -24,9 +29,11 @@ BEGIN
         IF RST = '0' THEN
             Q <= '0';
             QBAR <= '0';
-        ELSIF (CLK'EVENT AND CLK = '1' AND RST = '1') THEN
+        ELSIF (CLK'EVENT AND CLK = '1' AND RST = '1' AND ENA = '1') THEN
             Q <= D;
             QBAR <= NOT D;
+        ELSE
+            NULL; -- do nothing
         END IF;
     END PROCESS;
 
