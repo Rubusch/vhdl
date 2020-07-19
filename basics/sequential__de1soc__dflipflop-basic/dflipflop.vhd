cd34475ae51cd093e690c7252061fac21a9f4a93
Lothar Rubusch <l.rubusch@gmx.ch>
2020-07-20 00:00:01 +0200

    basics: dflipflop - basic version started

---
+++ basics/sequential__de1soc__dflipflop-basic/dflipflop.vhd
@@ -0,0 +1,27 @@
+-- basic d-flip-flop
+--
+-- Lothar Rubusch
+
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+
+ENTITY DFLIPFLOP IS
+PORT( CLK, RST : IN STD_LOGIC
+    ; D : IN STD_LOGIC
+    ; Q : OUT STD_LOGIC
+);
+END DFLIPFLOP;
+
+ARCHITECTURE ARCH OF DFLIPFLOP IS
+BEGIN
+    PROCESS(CLK, RST)
+    BEGIN
+        IF (RST = '1') THEN
+            Q <= '0';
+        ELSIF (CLK'EVENT AND CLK = '1') THEN
+            Q <= D;
+        ELSE
+            NULL;
+        END IF;
+    END PROCESS;
+END ARCH;
