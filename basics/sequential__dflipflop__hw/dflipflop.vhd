bab5f7c8a2e23409289cf48d9d7f05c33c2852ec
Lothar Rubusch <l.rubusch@gmx.ch>
2020-07-27 07:37:30 +0200

    basics: dflipflop-qbar - comments

--- basics/sequential__dflipflop__hw/dflipflop.vhd
+++ basics/sequential__dflipflop__hw/dflipflop.vhd
@@ -14,9 +14,10 @@ PORT( CLK : IN STD_LOGIC
     -- D-flip-flop on every 10th-clock, then we can set the
     -- enable to '1' for every 10thclock and '0' for rest of
     -- the clocks
-	; D : IN STD_LOGIC
-	; Q : OUT STD_LOGIC := '0'
-	; QBAR : OUT STD_LOGIC := '1'
+    ; D : IN STD_LOGIC
+    ; Q : OUT STD_LOGIC := '0'
+    ; QBAR : OUT STD_LOGIC := '1'
+    -- QBAR: returns the NOTed Q signal
 );
 END ENTITY DFLIPFLOP_ENT;
 
@@ -28,7 +29,7 @@ BEGIN
     BEGIN
         IF RST = '0' THEN
             Q <= '0';
-            QBAR <= '0';
+            QBAR <= '0'; -- set both to '0' as unpowered gate
         ELSIF (CLK'EVENT AND CLK = '1' AND RST = '1' AND ENA = '1') THEN
             Q <= D;
             QBAR <= NOT D;
