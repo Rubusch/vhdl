6d4164a677ac3666f72202da024fcbda95ed04e4
Lothar Rubusch <l.rubusch@gmx.ch>
2020-08-03 07:13:28 +0200

    basics: fsm_mealyandmoore - implementing

--- basics/fsm__mealy-and-moore/mealyandmoore.vhd
+++ basics/fsm__mealy-and-moore/mealyandmoore.vhd
@@ -1,5 +1,15 @@
 -- fsm: mealy and moore state machines
 --
+-- In Moore machines the output is available after 1 clock cycle and depends
+-- on the states only (synchronous machine).
+-- In Mealy machines the output is available as soon as the input is changed,
+-- and depends on the states along with external inputs (asynchronous machine).
+--
+-- In consequence Mealy machines require fewer states than Moore machines.
+-- Moore machine designs should be preferred where glitches are not a problem.
+-- Mealy machines are used in synchronous systems i.e. 'delay free' and
+-- 'glitch free'!
+--
 -- the example generates a tick of the duration of one clock cycle, whenever the input signal changes from 0 to 1.
 --
 --      +--+                         +--+
