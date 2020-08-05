f319859161380928f27928b8eba942f8dc5845b2
Lothar Rubusch <l.rubusch@gmx.ch>
2020-08-05 07:53:37 +0200

    basics: mooreregular - notes on regular moore machine

--- basics/fsm__moore-regular_TEMPLATES/mooreregular.vhd
+++ basics/fsm__moore-regular_TEMPLATES/mooreregular.vhd
@@ -4,7 +4,36 @@
 --
 -- Moore and Mealy machines can be divided into three categories i.e. ‘regular’, ‘timed’ and ‘recursive’.
 --
--- TODO ascii art
+--
+--
+-- REGULAR MOORE MACHINE
+--
+-- +---------+                    +---------+                    +---------+
+-- |  zero   |            x = '1' |  one    |            x = '1' |  two    |
+-- |         |------------------->|         |------------------->|         |
+-- | z = '0' |                    | z = '0' |                    | z = '1' |
+-- +---------+                    +---------+                    +---------+
+--
+--
+--
+-- TIMED MOORE MACHINE : next state depends on time as well
+--
+-- +---------+                    +---------+          x = '1' & +---------+
+-- |  zero   |            x = '1' |  one    |            t = 5   |  two    |
+-- |         |------------------->|         |------------------->|         |
+-- | z = '0' |                    | z = '0' |                    | z = '1' |
+-- +---------+                    +---------+                    +---------+
+--
+--
+--
+-- RECURSIVE MOORE MACHINE : output 'z' depends on output (feedback)
+--
+-- +---------+                    +---------+          x = '1' & +---------+
+-- |  zero   |            x = '1' |  one    |            z = 5   |  two    |
+-- |         |------------------->| z =     |------------------->|         |
+-- | z = '0' |                    |   z + 1 |                    | z = '1' |
+-- +---------+                    +---------+                    +---------+
+--
 --
 -- "Regular" means:
 -- * Output depends only on the states, therefore no ‘if statement’ is required in the process-statement.
@@ -32,8 +61,11 @@ PORT( CLK : IN STD_LOGIC
 END MOOREREGULAR;
 
 ARCHITECTURE ARCH OF MOOREREGULAR IS
+    TYPE STATETYPE IS (S0, S1, S2,...);
+    SIGNAL STATE_REG, STATE_NEXT : STATETYPE;
 
 BEGIN
 
+    
 END ARCH;
 
