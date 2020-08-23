e11aed8a6fe794144dde52c0e48971cfa5d1888c
Lothar Rubusch <l.rubusch@gmx.ch>
2020-08-23 21:52:12 +0200

    nios2: leds - implemented

--- nios2/nios2__leds/software/leds_app/main.c
+++ nios2/nios2__leds/software/leds_app/main.c
@@ -2,30 +2,26 @@
  * main.c
  *
  *  Created on: Aug 23, 2020
- *      Author: user
+ *      Author: Lothar Rubusch
  */
 
-#include "io.h"  // required for IOWR
-#include "alt_types.h" // required for alt_u8
-#include "system.h"  // contains address of LED_BASE
+#include "io.h"  /* IOWR() */
+#include "alt_types.h" /* alt_u8 */
+#include "system.h"  /* LED_BASE */
 
 int main(){
-	alt_u8 led_pattern = 0x01; // on the LED
+    alt_u8 led = 0x01;
 
-	//uncomment below line for visual verification of blinking LED
-	int i, itr=250000;
+    //uncomment below line for visual verification of blinking LED
+    int i, wait_period=250000;
 
-	printf("Blinking LED\n");
-	while(1){
-		printf("XXX\n");
+    printf("Blinky is running!\n");
+    while (1) {
+        led = ~led;
+        IOWR(LED_BASE, 0, led);
 
-		led_pattern = ~led_pattern; // not the led_pattern
-		IOWR(LED_BASE, 0, led_pattern); // write the led_pattern on LED
-		printf("YYY\n");
-
-		//uncomment 'for loop' in below line for visual verification of blinking LED
-		// dummy for loop to add delay in blinking, so that we can see the bliking.
-		for (i=0; i<itr; i ++){}
-		printf("ZZZ\n");
-  }
+        // some delay for
+        for (i=0; i < wait_period; i++)
+            ;
+    }
 }
