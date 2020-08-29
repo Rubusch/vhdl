7cfa1af0e497b350b8cc3a5ffa4f1c136d9ea559
Lothar Rubusch <l.rubusch@gmx.ch>
2020-08-29 23:28:01 +0200

    nios2: sdram university - add sine wave computation, sdram clocked via altera university pll clock

---
+++ nios2/nios2__sdram__university-clocks/software/sdram_sine_wave_app/main.c
@@ -0,0 +1,38 @@
+/*
+ * main.c
+ *
+ * IMPORTANT: needs full c libraries in BSP, not the reduced set (leaves out floating point types!)
+ *  Created on: Aug 29, 2020
+ *      Author: Lothar Rubusch
+ */
+
+#include <stdio.h>
+
+#include "io.h"
+#include "alt_types.h"
+#include "system.h"
+#include "math.h"
+
+int main()
+{
+//*
+	printf("SINE WAVE\n");
+	float i=0;
+    float sin_value;
+	float cos_value;
+
+	alt_u8 amplitude;
+
+    while (1) {
+        amplitude = IORD(SW_BASE, 0);
+        sin_value =  (int)amplitude * (float)sin(i);
+        cos_value =  (int)amplitude * (float)cos(i);
+
+        i = i + 0.01;
+        i = (i > 65536) ? 0 : i;
+
+        printf("%.3f,%.3f\n", sin_value, cos_value);
+    }
+
+    return 0;
+}
