91b11414e1aa9cb4703ba69eeb35410f7e01a91a
Lothar Rubusch <l.rubusch@gmx.ch>
2020-11-08 19:31:20 +0100

    nios2: memory test small - implemented with different types of memory, fixup

---
+++ nios2/altera-quartus__demo__memory-test/software/memory_test_small_bsp/system.h
@@ -0,0 +1,399 @@
+/*
+ * system.h - SOPC Builder system and BSP software package information
+ *
+ * Machine generated for CPU 'nios2_gen2_0' in SOPC Builder design 'nios2_de1soc'
+ * SOPC Builder design path: ../../nios2_de1soc.sopcinfo
+ *
+ * Generated: Sun Nov 08 19:06:01 CET 2020
+ */
+
+/*
+ * DO NOT MODIFY THIS FILE
+ *
+ * Changing this file will have subtle consequences
+ * which will almost certainly lead to a nonfunctioning
+ * system. If you do modify this file, be aware that your
+ * changes will be overwritten and lost when this file
+ * is generated again.
+ *
+ * DO NOT MODIFY THIS FILE
+ */
+
+/*
+ * License Agreement
+ *
+ * Copyright (c) 2008
+ * Altera Corporation, San Jose, California, USA.
+ * All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ * This agreement shall be governed in all respects by the laws of the State
+ * of California and by the laws of the United States of America.
+ */
+
+#ifndef __SYSTEM_H_
+#define __SYSTEM_H_
+
+/* Include definitions from linker script generator */
+#include "linker.h"
+
+
+/*
+ * CPU configuration
+ *
+ */
+
+#define ALT_CPU_ARCHITECTURE "altera_nios2_gen2"
+#define ALT_CPU_BIG_ENDIAN 0
+#define ALT_CPU_BREAK_ADDR 0x00000820
+#define ALT_CPU_CPU_ARCH_NIOS2_R1
+#define ALT_CPU_CPU_FREQ 50000000u
+#define ALT_CPU_CPU_ID_SIZE 1
+#define ALT_CPU_CPU_ID_VALUE 0x00000000
+#define ALT_CPU_CPU_IMPLEMENTATION "tiny"
+#define ALT_CPU_DATA_ADDR_WIDTH 0x1c
+#define ALT_CPU_DCACHE_LINE_SIZE 0
+#define ALT_CPU_DCACHE_LINE_SIZE_LOG2 0
+#define ALT_CPU_DCACHE_SIZE 0
+#define ALT_CPU_EXCEPTION_ADDR 0x0a000020
+#define ALT_CPU_FLASH_ACCELERATOR_LINES 0
+#define ALT_CPU_FLASH_ACCELERATOR_LINE_SIZE 0
+#define ALT_CPU_FLUSHDA_SUPPORTED
+#define ALT_CPU_FREQ 50000000
+#define ALT_CPU_HARDWARE_DIVIDE_PRESENT 0
+#define ALT_CPU_HARDWARE_MULTIPLY_PRESENT 0
+#define ALT_CPU_HARDWARE_MULX_PRESENT 0
+#define ALT_CPU_HAS_DEBUG_CORE 1
+#define ALT_CPU_HAS_DEBUG_STUB
+#define ALT_CPU_HAS_ILLEGAL_INSTRUCTION_EXCEPTION
+#define ALT_CPU_HAS_JMPI_INSTRUCTION
+#define ALT_CPU_ICACHE_LINE_SIZE 0
+#define ALT_CPU_ICACHE_LINE_SIZE_LOG2 0
+#define ALT_CPU_ICACHE_SIZE 0
+#define ALT_CPU_INST_ADDR_WIDTH 0x1c
+#define ALT_CPU_NAME "nios2_gen2_0"
+#define ALT_CPU_OCI_VERSION 1
+#define ALT_CPU_RESET_ADDR 0x0a000000
+
+
+/*
+ * CPU configuration (with legacy prefix - don't use these anymore)
+ *
+ */
+
+#define NIOS2_BIG_ENDIAN 0
+#define NIOS2_BREAK_ADDR 0x00000820
+#define NIOS2_CPU_ARCH_NIOS2_R1
+#define NIOS2_CPU_FREQ 50000000u
+#define NIOS2_CPU_ID_SIZE 1
+#define NIOS2_CPU_ID_VALUE 0x00000000
+#define NIOS2_CPU_IMPLEMENTATION "tiny"
+#define NIOS2_DATA_ADDR_WIDTH 0x1c
+#define NIOS2_DCACHE_LINE_SIZE 0
+#define NIOS2_DCACHE_LINE_SIZE_LOG2 0
+#define NIOS2_DCACHE_SIZE 0
+#define NIOS2_EXCEPTION_ADDR 0x0a000020
+#define NIOS2_FLASH_ACCELERATOR_LINES 0
+#define NIOS2_FLASH_ACCELERATOR_LINE_SIZE 0
+#define NIOS2_FLUSHDA_SUPPORTED
+#define NIOS2_HARDWARE_DIVIDE_PRESENT 0
+#define NIOS2_HARDWARE_MULTIPLY_PRESENT 0
+#define NIOS2_HARDWARE_MULX_PRESENT 0
+#define NIOS2_HAS_DEBUG_CORE 1
+#define NIOS2_HAS_DEBUG_STUB
+#define NIOS2_HAS_ILLEGAL_INSTRUCTION_EXCEPTION
+#define NIOS2_HAS_JMPI_INSTRUCTION
+#define NIOS2_ICACHE_LINE_SIZE 0
+#define NIOS2_ICACHE_LINE_SIZE_LOG2 0
+#define NIOS2_ICACHE_SIZE 0
+#define NIOS2_INST_ADDR_WIDTH 0x1c
+#define NIOS2_OCI_VERSION 1
+#define NIOS2_RESET_ADDR 0x0a000000
+
+
+/*
+ * Define for each module class mastered by the CPU
+ *
+ */
+
+#define __ALTERA_AVALON_JTAG_UART
+#define __ALTERA_AVALON_NEW_SDRAM_CONTROLLER
+#define __ALTERA_AVALON_ONCHIP_MEMORY2
+#define __ALTERA_AVALON_PIO
+#define __ALTERA_EPCQ_CONTROLLER2
+#define __ALTERA_NIOS2_GEN2
+
+
+/*
+ * System configuration
+ *
+ */
+
+#define ALT_DEVICE_FAMILY "Cyclone V"
+#define ALT_ENHANCED_INTERRUPT_API_PRESENT
+#define ALT_IRQ_BASE NULL
+#define ALT_LOG_PORT "/dev/null"
+#define ALT_LOG_PORT_BASE 0x0
+#define ALT_LOG_PORT_DEV null
+#define ALT_LOG_PORT_TYPE ""
+#define ALT_NUM_EXTERNAL_INTERRUPT_CONTROLLERS 0
+#define ALT_NUM_INTERNAL_INTERRUPT_CONTROLLERS 1
+#define ALT_NUM_INTERRUPT_CONTROLLERS 1
+#define ALT_STDERR "/dev/jtag"
+#define ALT_STDERR_BASE 0xc081060
+#define ALT_STDERR_DEV jtag
+#define ALT_STDERR_IS_JTAG_UART
+#define ALT_STDERR_PRESENT
+#define ALT_STDERR_TYPE "altera_avalon_jtag_uart"
+#define ALT_STDIN "/dev/jtag"
+#define ALT_STDIN_BASE 0xc081060
+#define ALT_STDIN_DEV jtag
+#define ALT_STDIN_IS_JTAG_UART
+#define ALT_STDIN_PRESENT
+#define ALT_STDIN_TYPE "altera_avalon_jtag_uart"
+#define ALT_STDOUT "/dev/jtag"
+#define ALT_STDOUT_BASE 0xc081060
+#define ALT_STDOUT_DEV jtag
+#define ALT_STDOUT_IS_JTAG_UART
+#define ALT_STDOUT_PRESENT
+#define ALT_STDOUT_TYPE "altera_avalon_jtag_uart"
+#define ALT_SYSTEM_NAME "nios2_de1soc"
+
+
+/*
+ * epcq256_avl_csr configuration
+ *
+ */
+
+#define ALT_MODULE_CLASS_epcq256_avl_csr altera_epcq_controller2
+#define EPCQ256_AVL_CSR_BASE 0xc081000
+#define EPCQ256_AVL_CSR_FLASH_TYPE "EPCQ256"
+#define EPCQ256_AVL_CSR_IRQ 0
+#define EPCQ256_AVL_CSR_IRQ_INTERRUPT_CONTROLLER_ID 0
+#define EPCQ256_AVL_CSR_IS_EPCS 0
+#define EPCQ256_AVL_CSR_NAME "/dev/epcq256_avl_csr"
+#define EPCQ256_AVL_CSR_NUMBER_OF_SECTORS 512
+#define EPCQ256_AVL_CSR_PAGE_SIZE 256
+#define EPCQ256_AVL_CSR_SECTOR_SIZE 65536
+#define EPCQ256_AVL_CSR_SPAN 64
+#define EPCQ256_AVL_CSR_SUBSECTOR_SIZE 4096
+#define EPCQ256_AVL_CSR_TYPE "altera_epcq_controller2"
+
+
+/*
+ * epcq256_avl_mem configuration
+ *
+ */
+
+#define ALT_MODULE_CLASS_epcq256_avl_mem altera_epcq_controller2
+#define EPCQ256_AVL_MEM_BASE 0xa000000
+#define EPCQ256_AVL_MEM_FLASH_TYPE "EPCQ256"
+#define EPCQ256_AVL_MEM_IRQ -1
+#define EPCQ256_AVL_MEM_IRQ_INTERRUPT_CONTROLLER_ID -1
+#define EPCQ256_AVL_MEM_IS_EPCS 0
+#define EPCQ256_AVL_MEM_NAME "/dev/epcq256_avl_mem"
+#define EPCQ256_AVL_MEM_NUMBER_OF_SECTORS 512
+#define EPCQ256_AVL_MEM_PAGE_SIZE 256
+#define EPCQ256_AVL_MEM_SECTOR_SIZE 65536
+#define EPCQ256_AVL_MEM_SPAN 33554432
+#define EPCQ256_AVL_MEM_SUBSECTOR_SIZE 4096
+#define EPCQ256_AVL_MEM_TYPE "altera_epcq_controller2"
+
+
+/*
+ * hal configuration
+ *
+ */
+
+#define ALT_INCLUDE_INSTRUCTION_RELATED_EXCEPTION_API
+#define ALT_MAX_FD 4
+#define ALT_SYS_CLK none
+#define ALT_TIMESTAMP_CLK none
+
+
+/*
+ * jtag configuration
+ *
+ */
+
+#define ALT_MODULE_CLASS_jtag altera_avalon_jtag_uart
+#define JTAG_BASE 0xc081060
+#define JTAG_IRQ 1
+#define JTAG_IRQ_INTERRUPT_CONTROLLER_ID 0
+#define JTAG_NAME "/dev/jtag"
+#define JTAG_READ_DEPTH 64
+#define JTAG_READ_THRESHOLD 8
+#define JTAG_SPAN 8
+#define JTAG_TYPE "altera_avalon_jtag_uart"
+#define JTAG_WRITE_DEPTH 64
+#define JTAG_WRITE_THRESHOLD 8
+
+
+/*
+ * led configuration
+ *
+ */
+
+#define ALT_MODULE_CLASS_led altera_avalon_pio
+#define LED_BASE 0xc081050
+#define LED_BIT_CLEARING_EDGE_REGISTER 0
+#define LED_BIT_MODIFYING_OUTPUT_REGISTER 0
+#define LED_CAPTURE 0
+#define LED_DATA_WIDTH 8
+#define LED_DO_TEST_BENCH_WIRING 0
+#define LED_DRIVEN_SIM_VALUE 0
+#define LED_EDGE_TYPE "NONE"
+#define LED_FREQ 50000000
+#define LED_HAS_IN 0
+#define LED_HAS_OUT 1
+#define LED_HAS_TRI 0
+#define LED_IRQ -1
+#define LED_IRQ_INTERRUPT_CONTROLLER_ID -1
+#define LED_IRQ_TYPE "NONE"
+#define LED_NAME "/dev/led"
+#define LED_RESET_VALUE 0
+#define LED_SPAN 16
+#define LED_TYPE "altera_avalon_pio"
+
+
+/*
+ * ram0 configuration
+ *
+ */
+
+#define ALT_MODULE_CLASS_ram0 altera_avalon_onchip_memory2
+#define RAM0_ALLOW_IN_SYSTEM_MEMORY_CONTENT_EDITOR 0
+#define RAM0_ALLOW_MRAM_SIM_CONTENTS_ONLY_FILE 0
+#define RAM0_BASE 0xc060000
+#define RAM0_CONTENTS_INFO ""
+#define RAM0_DUAL_PORT 0
+#define RAM0_GUI_RAM_BLOCK_TYPE "AUTO"
+#define RAM0_INIT_CONTENTS_FILE "nios2_de1soc_ram0"
+#define RAM0_INIT_MEM_CONTENT 1
+#define RAM0_INSTANCE_ID "NONE"
+#define RAM0_IRQ -1
+#define RAM0_IRQ_INTERRUPT_CONTROLLER_ID -1
+#define RAM0_NAME "/dev/ram0"
+#define RAM0_NON_DEFAULT_INIT_FILE_ENABLED 0
+#define RAM0_RAM_BLOCK_TYPE "AUTO"
+#define RAM0_READ_DURING_WRITE_MODE "DONT_CARE"
+#define RAM0_SINGLE_CLOCK_OP 0
+#define RAM0_SIZE_MULTIPLE 1
+#define RAM0_SIZE_VALUE 131072
+#define RAM0_SPAN 131072
+#define RAM0_TYPE "altera_avalon_onchip_memory2"
+#define RAM0_WRITABLE 1
+
+
+/*
+ * ram1 configuration
+ *
+ */
+
+#define ALT_MODULE_CLASS_ram1 altera_avalon_onchip_memory2
+#define RAM1_ALLOW_IN_SYSTEM_MEMORY_CONTENT_EDITOR 0
+#define RAM1_ALLOW_MRAM_SIM_CONTENTS_ONLY_FILE 0
+#define RAM1_BASE 0xc040000
+#define RAM1_CONTENTS_INFO ""
+#define RAM1_DUAL_PORT 0
+#define RAM1_GUI_RAM_BLOCK_TYPE "AUTO"
+#define RAM1_INIT_CONTENTS_FILE "nios2_de1soc_ram1"
+#define RAM1_INIT_MEM_CONTENT 1
+#define RAM1_INSTANCE_ID "NONE"
+#define RAM1_IRQ -1
+#define RAM1_IRQ_INTERRUPT_CONTROLLER_ID -1
+#define RAM1_NAME "/dev/ram1"
+#define RAM1_NON_DEFAULT_INIT_FILE_ENABLED 0
+#define RAM1_RAM_BLOCK_TYPE "AUTO"
+#define RAM1_READ_DURING_WRITE_MODE "DONT_CARE"
+#define RAM1_SINGLE_CLOCK_OP 0
+#define RAM1_SIZE_MULTIPLE 1
+#define RAM1_SIZE_VALUE 131072
+#define RAM1_SPAN 131072
+#define RAM1_TYPE "altera_avalon_onchip_memory2"
+#define RAM1_WRITABLE 1
+
+
+/*
+ * sdram configuration
+ *
+ */
+
+#define ALT_MODULE_CLASS_sdram altera_avalon_new_sdram_controller
+#define SDRAM_BASE 0x4000000
+#define SDRAM_CAS_LATENCY 3
+#define SDRAM_CONTENTS_INFO
+#define SDRAM_INIT_NOP_DELAY 0.0
+#define SDRAM_INIT_REFRESH_COMMANDS 2
+#define SDRAM_IRQ -1
+#define SDRAM_IRQ_INTERRUPT_CONTROLLER_ID -1
+#define SDRAM_IS_INITIALIZED 1
+#define SDRAM_NAME "/dev/sdram"
+#define SDRAM_POWERUP_DELAY 100.0
+#define SDRAM_REFRESH_PERIOD 15.625
+#define SDRAM_REGISTER_DATA_IN 1
+#define SDRAM_SDRAM_ADDR_WIDTH 0x19
+#define SDRAM_SDRAM_BANK_WIDTH 2
+#define SDRAM_SDRAM_COL_WIDTH 10
+#define SDRAM_SDRAM_DATA_WIDTH 16
+#define SDRAM_SDRAM_NUM_BANKS 4
+#define SDRAM_SDRAM_NUM_CHIPSELECTS 1
+#define SDRAM_SDRAM_ROW_WIDTH 13
+#define SDRAM_SHARED_DATA 0
+#define SDRAM_SIM_MODEL_BASE 0
+#define SDRAM_SPAN 67108864
+#define SDRAM_STARVATION_INDICATOR 0
+#define SDRAM_TRISTATE_BRIDGE_SLAVE ""
+#define SDRAM_TYPE "altera_avalon_new_sdram_controller"
+#define SDRAM_T_AC 5.5
+#define SDRAM_T_MRD 3
+#define SDRAM_T_RCD 20.0
+#define SDRAM_T_RFC 70.0
+#define SDRAM_T_RP 20.0
+#define SDRAM_T_WR 14.0
+
+
+/*
+ * sw configuration
+ *
+ */
+
+#define ALT_MODULE_CLASS_sw altera_avalon_pio
+#define SW_BASE 0xc081040
+#define SW_BIT_CLEARING_EDGE_REGISTER 0
+#define SW_BIT_MODIFYING_OUTPUT_REGISTER 0
+#define SW_CAPTURE 0
+#define SW_DATA_WIDTH 8
+#define SW_DO_TEST_BENCH_WIRING 0
+#define SW_DRIVEN_SIM_VALUE 0
+#define SW_EDGE_TYPE "NONE"
+#define SW_FREQ 50000000
+#define SW_HAS_IN 1
+#define SW_HAS_OUT 0
+#define SW_HAS_TRI 0
+#define SW_IRQ -1
+#define SW_IRQ_INTERRUPT_CONTROLLER_ID -1
+#define SW_IRQ_TYPE "NONE"
+#define SW_NAME "/dev/sw"
+#define SW_RESET_VALUE 0
+#define SW_SPAN 16
+#define SW_TYPE "altera_avalon_pio"
+
+#endif /* __SYSTEM_H_ */
